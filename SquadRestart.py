d8f15dda92f04b7d92476624c7a63ef7a9aa5532
mrkaland95 <56325067+mrkaland95@users.noreply.github.com>
2021-04-13 22:47:22 +0200

    Add files via upload

--- SquadRestart.py
+++ SquadRestart.py
@@ -4,7 +4,7 @@ import a2s
 
 seedingThreshold = 40 # The threshold at which the pc will shut down
 address = ("r2f.tacticaltriggernometry.com", 27165)
-userChoice = input("Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game")
+userChoice = input("Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game \n")
 #while not userChoice.lower() == "shutdown" or userChoice.lower() == "close":
 while True:
     seedingThreshold = 40
@@ -14,11 +14,10 @@ while True:
         if not player.name == "":
             players.append(player)
     playercount = len(players)
-    print(playercount)
+    print("The player count is currently %d " % playercount)
     if playercount >= seedingThreshold:
-        print("Above seeding threshold")
-        if userChoice.lower() == "shutdown":
-            print("shutting down")
+        print("Above seeding threshold \nShutting down")
+        #      os.system("shutdown /s /t 1")
     time.sleep(60)
 
 
@@ -26,7 +25,7 @@ while True:
 #shutdown = input("Do you wish to shutdown your computer ? (Yes / No ) \n ")
 #if isinstance(shutdown, str):
  #   if shutdown.lower() == "yes":
-  #      os.system("shutdown /s /t 1")
+
 #else:
  #   raise Exception("Not a valid input, must be a string of letters")
 
