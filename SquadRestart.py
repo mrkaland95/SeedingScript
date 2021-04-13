c6c975d9790c03b42543f6d83de8bc46bd422233
Steffen <mrkaland95@gmail.com>
2021-04-13 22:14:37 +0200

    Initial work on the script

--- SquadRestart.py
+++ SquadRestart.py
@@ -1,123 +1,34 @@
-import os, time, a2s,sys
-import datetime
-from tkinter import *
+import os
+import time
+import a2s
 
-sleeptime = 60  # How often the script will query the server, measured in seconds.
-seedingThreshold = 50  # The critical player threshold at which the chosen action will be taken.
+seedingThreshold = 40 # The threshold at which the pc will shut down
 address = ("r2f.tacticaltriggernometry.com", 27165)
-gameexecutable = "SquadGame.exe"
-userinput = ""
-
-
-class GUI():
-    def __init__(self):
-        def shutdownbutton():
-            global buttoninput # Stupid as hell, but seems it had to be done this way
-            buttoninput = "shutdown"
-            print("Your computer will be shut down upon reaching the seeding threshold")
-            closeGUI(self)
-
-        def gameclosebutton():
-            global buttoninput
-            buttoninput = "close"
-            print("The game will be closed upon hitting the seeding threshold")
-            closeGUI(self)
-
-        def closeGUI(self):
-            self.root.destroy()
-
-        self.root = Tk()
-        self.root.title("Seeding script")
-        button1 = Button(self.root, text="Shutdown the computer upon reaching the threshold", padx=31,pady=50,command=shutdownbutton)
-        button2 = Button(self.root, text="Close down the game upon reaching the threshold     ",padx=30,pady=50,command=gameclosebutton)
-        button1.grid(row=1, column=0)
-        button2.grid(row=2, column=0)
-        window = Label(self.root, text="SeedingScript")
-        window.mainloop()
-
-
-def choicetoexecute(userchoice, playercount, threshold, executable):
-    if userchoice == "close":
-        gameclose(playercount, threshold, executable)
-    elif userchoice == "shutdown":
-        shutdown(playercount, threshold)
-
-
-
-
-def gameclose(playercount, threshold, executable):
-    """
-    Function that shuts down the game when the playercount reaches the critical threshold.
-    """
-    if playercount >= threshold:
-        print("Closing down the game")
-        os.system("TASKKILL /F /IM %s" % executable)
-        sys.exit()
-
-
-def shutdown(playercount, threshold):
-    """
-    Shuts down the computer upon hitting the desired threshold
-    :param playercount:
-    :param threshold:
-    :return:
-    """
-    if playercount >= threshold:
-        print("Shutting down the computer")
-        os.system("shutdown /s /t 1")
-
-
-def playercount(server):
-    """
-    The amount of players that are actively loaded in to the server
-    :param the server a2s server address that will be queried:
-    :return:
-    """
-    serverplayers = a2s.players(server)
+userChoice = input("Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game")
+#while not userChoice.lower() == "shutdown" or userChoice.lower() == "close":
+while True:
+    seedingThreshold = 40
+    serverplayer = a2s.players(address)
     players = []
-    for player in serverplayers:
-        if player.name != "":
+    for player in (serverplayer):
+        if not player.name == "":
             players.append(player)
-    return len(players)
+    playercount = len(players)
+    print(playercount)
+    if playercount >= seedingThreshold:
+        print("Above seeding threshold")
+        if userChoice.lower() == "shutdown":
+            print("shutting down")
+    time.sleep(60)
 
 
 
-def argstaken():
-    """
-    Checks if there were any arguments supplied from the command line, if applicable
-    :return:
-    """
-    try:
-        args = sys.argv[1:][0]
-        if args == "close" or args == "shutdown":
-            print("The argument input was '%s'" % args)
-            return args
-        else:
-            raise Exception("You must input a valid argument")
-
-    except IndexError:  # The program will throw an indexerror if no arguments were supplied on start.
-        return False
+#shutdown = input("Do you wish to shutdown your computer ? (Yes / No ) \n ")
+#if isinstance(shutdown, str):
+ #   if shutdown.lower() == "yes":
+  #      os.system("shutdown /s /t 1")
+#else:
+ #   raise Exception("Not a valid input, must be a string of letters")
 
 
 
-def main(sleeptime):
-    userinput = argstaken()
-    if not userinput:
-        GUI() # Creates the gui object and runs it. Terminates upon button click
-        userinput = buttoninput
-    while True:
-        try:
-            now = datetime.datetime.now()
-            current_time = now.strftime("%H:%M")
-            player_count = playercount(address)
-            print(current_time + "  --  Currently %d players on the server" % player_count)
-            choicetoexecute(userinput, player_count, seedingThreshold, gameexecutable)
-        except Exception as exception:
-            print(exception)
-            pass
-        time.sleep(sleeptime)
-
-
-
-if __name__ == '__main__':
-    main(sleeptime)
