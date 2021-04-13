0c4257070331742d527615c1ee30b708e30c7bd7
Steffen <mrkaland95@gmail.com>
2021-04-13 22:14:37 +0200

    Initial work on the script

--- SquadRestart.py
+++ SquadRestart.py
@@ -1,41 +1,34 @@
 import os
 import time
 import a2s
-import sys
 
-seedingThreshold = 40 # The critical player threshold at which the chosen action will be taken.
+seedingThreshold = 40 # The threshold at which the pc will shut down
 address = ("r2f.tacticaltriggernometry.com", 27165)
-args = str(sys.argv)
-userChoice = args[1:2]
-print(userChoice)
-
-
-def main(args):
-        #while True:
-         #   userChoice = input("Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game \n")
-          #  if not userChoice.lower() == "shutdown" or userChoice.lower() == "close":
-           #     print("Please input a valid string. Either 'shutdown' or 'close'. CTRL+C to abort the program")
-            #    break
-    while True:
-        seedingThreshold = 40
-        serverplayers = a2s.players(address)
-        players = []
-        for player in (serverplayers):
-            if not player.name == "":
-                players.append(player)
-        playercount = len(players)
-        print(playercount)
-        if playercount >= seedingThreshold:
-            print("Above seeding threshold")
-            if userChoice.lower() == "shutdown":
-                print("Shutting down the computer")
-                os.system("shutdown /s /t 1")
-                break
-            elif userChoice.lower() == "close":
-                print("Closing down the game")
-                os.system("TASKKILL /F /IM SquadGame.Exe")
-                break
+userChoice = input("Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game")
+#while not userChoice.lower() == "shutdown" or userChoice.lower() == "close":
+while True:
+    seedingThreshold = 40
+    serverplayer = a2s.players(address)
+    players = []
+    for player in (serverplayer):
+        if not player.name == "":
+            players.append(player)
+    playercount = len(players)
+    print(playercount)
+    if playercount >= seedingThreshold:
+        print("Above seeding threshold")
+        if userChoice.lower() == "shutdown":
+            print("shutting down")
     time.sleep(60)
 
-if __name__ == '__main__':
-    main(args)
+
+
+#shutdown = input("Do you wish to shutdown your computer ? (Yes / No ) \n ")
+#if isinstance(shutdown, str):
+ #   if shutdown.lower() == "yes":
+  #      os.system("shutdown /s /t 1")
+#else:
+ #   raise Exception("Not a valid input, must be a string of letters")
+
+
+
