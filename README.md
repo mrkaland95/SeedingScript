6046a539aeff43eb69e17b2a5606bbc8825ba73f
Steffen <mrkaland95@gmail.com>
2021-04-14 15:34:15 +0200

    Working on button

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SeedingScript
 Script for shutting down the game/PC when the server population is high enough
+
+To run, you must install the python interpreter.
+
+https://www.python.org/downloads/
+
+
+
+Once installed, search for CMD in windows, and input the following command
+
+"pip3 install python-a2s"
+
+
--- SquadRestart.py
+++ SquadRestart.py
@@ -1,28 +1,73 @@
 import os, time, a2s, sys
 import datetime
+import tkinter as tk
 
-
-
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
 sleeptime = 60  # How often the script will query the server, meausured in seconds.
 seedingThreshold = 45  # The critical player threshold at which the chosen action will be taken.
 address = ("r2f.tacticaltriggernometry.com", 27165)
 validInputs = ["close", "shutdown"]
 
 
-while True:
-    try:
-        args = sys.argv[1:][0]
-        userChoice = args
-        if userChoice != "":
-            break
-    except IndexError:
-        userChoice = input(
-        "Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game \n"
-        )
-        if not userChoice.lower() in validInputs:
-            print("Please input a valid string. Either 'shutdown' or 'close'. CTRL+C to abort the program")
-        else:
-            break
+close = tk.Button(frame,
+                   text="QUIT",
+                   fg="red",
+                   command=close())
+button.pack(side=tk.LEFT)
+shutdown = tk.Button(frame,
+                   text="Hello",
+                   command=shutdown())
+slogan.pack(side=tk.LEFT)
+
+
+
+def close():
+
+
+
+
+
+def shutdown(treshold, sleeptime):
+    while True:
+        try:
+            now = datetime.datetime.now()
+            current_time = now.strftime("%H:%M")
+            serverplayers = a2s.players(address)
+            players = []
+            for player in serverplayers:
+                if player.name != "":
+                    players.append(player)
+            playercount = len(players)
+            print(current_time + "  --  Currently %d players on the server" % playercount)
+            if playercount >= treshold:
+                print("Shutting down the computer")
+                os.system("shutdown /s /t 1")
+            except Exception:
+            pass
+        time.sleep(sleeptime)
+
+
+
+
+def userprompt():
+    while True:
+        try:
+            args = sys.argv[1:][0]
+            userChoice = args
+            if userChoice != "":
+                break
+        except IndexError:
+            userChoice = input(
+            "Input 'shutdown' if you wish your computer to shut off, or input 'close' to close the game,"
+            " upon hitting the threshold"
+            " \n"
+            )
+            if not userChoice.lower() in validInputs:
+                print("Please input a valid string. Either 'shutdown' or 'close'. CTRL+C to abort the program")
+            else:
+                break
 
 
 
