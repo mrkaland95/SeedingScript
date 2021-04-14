d28cd474fe87c2ccb7702d69713abf6dc605af7b
mrkaland95 <mrkaland95@gmail.com>
2021-04-15 01:34:16 +0200

    Working script with GUI. Still small things to optimize, but works

--- SquadRestart.py
+++ SquadRestart.py
@@ -1,33 +1,45 @@
-import os, time, a2s, sys
+import os, time, a2s,sys
 import datetime
 from tkinter import *
 
-sleeptime = 5  # How often the script will query the server, measured in seconds.
+sleeptime = 60  # How often the script will query the server, measured in seconds.
 seedingThreshold = 50  # The critical player threshold at which the chosen action will be taken.
 address = ("r2f.tacticaltriggernometry.com", 27165)
 gameexecutable = "SquadGame.exe"
 userinput = ""
 
 
-def GUI():
-    """
-    Initiates the GUI
-    :return:
-    """
-    top = Tk()
-    shutdownbutton = Button(top, text="Shutdown computer", padx=50,pady=50)
-    gameclosebutton = Button(top, text="Close down game",padx=50,pady=50)
-    shutdownbutton.grid(row=1, collumn=0)
-    gameclosebutton.grid(row=2, collumn=0)
-    gameclosebutton.pack()
-    shutdownbutton.pack()
-    window = Label(top, text="SeedingScript")
-    window.pack()
-    window.mainloop()
+class GUI():
+    def __init__(self):
+        def shutdownbutton():
+            global buttoninput # Stupid as hell, but seems it had to be done this way
+            buttoninput = "shutdown"
+            print("Your computer will be shut down upon reaching the seeding threshold")
+            closeGUI(self)
 
-def shutdownbutton():
-    global userinput = "shutdown"
-    return
+        def gameclosebutton():
+            global buttoninput
+            buttoninput = "close"
+            print("The game will be closed upon hitting the seeding threshold")
+            closeGUI(self)
+
+        def closeGUI(self):
+            self.root.destroy()
+
+        self.root = Tk()
+        button1 = Button(self.root, text="Shutdown computer", padx=30,pady=50,command=shutdownbutton)
+        button2 = Button(self.root, text="Close down game     ",padx=30,pady=50,command=gameclosebutton)
+        button1.grid(row=1, column=0)
+        button2.grid(row=2, column=0)
+        window = Label(self.root, text="SeedingScript")
+        window.mainloop()
+
+
+def choicetoexecute(userchoice, playercount, threshold, executable):
+    if userchoice == "close":
+        gameclose(playercount, threshold, executable)
+    elif userchoice == "shutdown":
+        shutdown(playercount, threshold)
 
 
 
@@ -39,10 +51,7 @@ def gameclose(playercount, threshold, executable):
     if playercount >= threshold:
         print("Closing down the game")
         os.system("TASKKILL /F /IM %s" % executable)
-    return
-
-
-
+        sys.exit()
 
 
 def shutdown(playercount, threshold):
@@ -55,7 +64,6 @@ def shutdown(playercount, threshold):
     if playercount >= threshold:
         print("Shutting down the computer")
         os.system("shutdown /s /t 1")
-    return
 
 
 def playercount(server):
@@ -80,22 +88,30 @@ def argstaken():
     """
     try:
         args = sys.argv[1:][0]
-        if args != "":
+        if args == "close" or args == "shutdown":
+            print("The argument input was '%s'" % args)
             return args
+        else:
+            raise Exception("You must input a valid argument")
+
     except IndexError:  # The program will throw an indexerror if no arguments were supplied on start.
         return False
 
 
 
 def main(sleeptime):
-    GUI()
+    userinput = argstaken()
+    if not userinput:
+        GUI() # Creates the gui object and runs it. Terminates upon button click
+        userinput = buttoninput
     while True:
         try:
             now = datetime.datetime.now()
             current_time = now.strftime("%H:%M")
             player_count = playercount(address)
             print(current_time + "  --  Currently %d players on the server" % player_count)
-
+            print(buttoninput)
+            choicetoexecute(userinput, player_count, seedingThreshold, gameexecutable)
         except Exception as exception:
             print(exception)
             pass
